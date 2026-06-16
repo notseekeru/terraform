867b034af6c7665ebfbdcdb51208e57b63ba74a0
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-16 10:33:10 +0800

    feat: flake for reproduce

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Kubectl, Terraform, and ArgoCD";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        kubectl
+        terraform
+        argocd
+      ];
+    };
+  };
+}
