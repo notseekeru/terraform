95fa403a0131a49c64e5cd20289db1f7d2cfbd4a
notseekeru <myteseeker18@gmail.com>
2026-06-18 13:20:49 +0800

    feat: helm and argocd and ingress

--- infrastructure/kubernetes.tf
+++ infrastructure/kubernetes.tf
@@ -22,6 +22,12 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  host                   = digitalocean_kubernetes_cluster.lab_cluster.endpoint
+  token                  = digitalocean_kubernetes_cluster.lab_cluster.kube_config.0.token
+  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.lab_cluster.kube_config.0.cluster_ca_certificate)
+}
+
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
@@ -29,4 +35,89 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   create_namespace = true
   depends_on       = [digitalocean_kubernetes_cluster.lab_cluster]
+}
+
+resource "kubernetes_manifest" "gitops_app" {
+  depends_on = [helm_release.argocd]  # wait for ArgoCD to be installed
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "gitops"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/notseekeru/gitops.git"
+        path           = "."
+        targetRevision = "main"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "default"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "nginx_ingress" {
+  depends_on = [helm_release.argocd]
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "ingress-nginx"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL = "https://kubernetes.github.io/ingress-nginx"
+        chart   = "ingress-nginx"
+        targetRevision = "4.11.3"
+        helm = {
+          values = <<-EOT
+            controller:
+              service:
+                type: LoadBalancer
+          EOT
+        }
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "ingress-nginx"
+      }
+      syncPolicy = {
+        syncOptions = [ "CreateNamespace=true" ]
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_secret" "cloudflare_token" {
+  depends_on = [digitalocean_kubernetes_cluster.lab_cluster]
+
+  metadata {
+    name      = "cloudflared-token"
+    namespace = "default"
+  }
+
+  data = {
+    token = var.cloudflare_tunnel_token   # you must define this variable
+  }
+
+  type = "Opaque"
 }
--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -1,9 +1,21 @@
 terraform {
   required_version = ">= 1.0"
-
+  
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubernetes = {          # add this
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+    local = {
+      source  = "hashicorp/local"
       version = "~> 2.0"
     }
   }
