42288065bd543f9a5a03eb6e1ed9bd1326fc2618
notseekeru <myteseeker18@gmail.com>
2026-06-18 13:08:11 +0800

    feat: argocd and helm in kubernetes

--- infrastructure/kubernetes.tf
+++ infrastructure/kubernetes.tf
@@ -2,7 +2,6 @@ resource "digitalocean_kubernetes_cluster" "lab_cluster" {
   name     = "lab-cluster"
   region   = var.default_region
   version  = "1.34.8-do.1"
-
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-2gb"
@@ -11,6 +10,23 @@ resource "digitalocean_kubernetes_cluster" "lab_cluster" {
 }
 
 resource "local_file" "kubeconfig" {
-  filename = "~/kubeconfig"
+  filename = pathexpand("~/kubeconfig")
   content  = digitalocean_kubernetes_cluster.lab_cluster.kube_config.0.raw_config
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = digitalocean_kubernetes_cluster.lab_cluster.endpoint
+    token                  = digitalocean_kubernetes_cluster.lab_cluster.kube_config.0.token
+    cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.lab_cluster.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  depends_on       = [digitalocean_kubernetes_cluster.lab_cluster]
 }
