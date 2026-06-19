073619fb4784442dad4fc97617f4e23603e5ab00
notseekeru <myteseeker18@gmail.com>
2026-06-20 00:58:52 +0800

    fix: false config kubectl

--- infrastructure/kubernetes.tf
+++ infrastructure/kubernetes.tf
@@ -36,7 +36,9 @@ provider "kubectl" {
   host                   = digitalocean_kubernetes_cluster.lab_cluster.endpoint
   token                  = digitalocean_kubernetes_cluster.lab_cluster.kube_config.0.token
   cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.lab_cluster.kube_config.0.cluster_ca_certificate)
+  load_config_file       = false
 }
+
 
 # --- ArgoCD Installation ---
 
