ae5b41028163ef53fdeaecd9878afa49e06ad94a
notseekeru <myteseeker18@gmail.com>
2026-06-20 21:12:38 +0800

    refactor: merge droplet relevant files to a single file

--- infrastructure/droplet.tf
+++ infrastructure/droplet.tf
@@ -1,3 +1,36 @@
+variable "ssh_public_keys" {
+  type = map(string)
+}
+
+variable "default_region" {
+  type        = string
+  default     = "sgp1"
+  description = "Default DigitalOcean region for servers that don't specify one"
+}
+
+variable "default_size" {
+  type    = string
+  default = "s-1vcpu-1gb"
+}
+
+variable "default_image" {
+  type    = string
+  default = "debian-13-x64"
+}
+
+variable "servers" {
+  type = map(object({
+    image  = optional(string)
+    region = optional(string)
+    size   = optional(string)
+    tags   = optional(list(string))
+  }))
+
+  default = {
+    # N/A
+  }
+}
+
 resource "digitalocean_ssh_key" "this" {
   for_each   = var.ssh_public_keys
   name       = each.key
@@ -40,4 +73,23 @@ resource "local_file" "ansible_inventory" {
   depends_on = [
     digitalocean_droplet.this
   ]
+}
+
+output "droplets" {
+  value = {
+    for k, d in digitalocean_droplet.this : k => {
+      ipv4_address = d.ipv4_address
+      ipv6_address = d.ipv6_address
+      urn          = d.urn
+      region       = d.region
+      size         = d.size
+      image        = d.image
+      tags         = d.tags
+    }
+  }
+}
+
+output "droplet_ips" {
+  value       = { for k, d in digitalocean_droplet.this : k => d.ipv4_address }
+  description = "Map of server name → public IPv4 address"
 }
