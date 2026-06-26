45e90ad94d991e5822090f4683d3b5fc8aca53f7
Stephen Macabulos <myteseeker18@gmail.com>
2026-06-26 13:41:44 +0800

    feat: doctl in flake nix to measure destroy time

--- flake.nix
+++ flake.nix
@@ -2,16 +2,19 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        kubectl
-        terraform
-        argocd
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          kubectl
+          terraform
+          argocd
+          doctl
+        ];
+      };
     };
-  };
 }
