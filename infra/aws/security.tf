c1c4bf95eba1a138cf40d0d7b4794a9b905f74a5
Stephen Macabulos <myteseeker18@gmail.com>
2026-08-28 18:15:16 +0800

    fix(aws): grant EC2 SSM access to /app/* params

--- infra/aws/security.tf
+++ infra/aws/security.tf
@@ -54,9 +54,28 @@ resource "aws_iam_role" "ec2_ssm" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
       Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_ssm_params" {
+  name = "ec2-ssm-app-params"
+  role = aws_iam_role.ec2_ssm.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid    = "AllowGetAppParams"
+      Effect = "Allow"
+      Action = [
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+        "ssm:GetParametersByPath",
+      ]
+      Resource = "arn:aws:ssm:${var.region}:*:parameter/app/*"
     }]
   })
 }
