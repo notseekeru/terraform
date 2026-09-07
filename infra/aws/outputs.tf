97ab2841d506a98df29a1573eb2cdc32df8a34a3
Stephen Macabulos <myteseeker18@gmail.com>
2026-09-07 08:05:11 +0800

    docs(aws): mark validation-cname output informational

--- infra/aws/outputs.tf
+++ infra/aws/outputs.tf
@@ -9,7 +9,7 @@ output "cloudfront_domain" {
 }
 
 output "alb_domain_validation_cname" {
-  description = "CNAME to add in Cloudflare to validate the ACM HTTPS certificate (empty if no domain set)"
+  description = "Informational: the ACM validation CNAME(s) (auto-created in Cloudflare by this module when alb_domain is set; empty if no domain)"
   value = var.alb_domain != "" ? [
     for r in aws_acm_certificate.alb[0].domain_validation_options : {
       name  = r.resource_record_name
